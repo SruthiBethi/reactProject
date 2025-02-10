d43c3d1f6ed6010e27bd8956ad53cc8e334fad17
SruthiBethi <bethisruthi35@gmail.com>
2025-02-10 15:07:14 +0530

    first commit

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -15,7 +15,7 @@ function NotFound() {
     return (
         <div className="container text-center mt-5">
             <h1 className="display-3 text-danger">404 Page Not Found</h1>
-            <p className="lead">Navigating to Home Page in 5 seconds...</p>
+            <p className="lead">Navigating to Home Page in 5 seconds...,</p>
             <img src='/notfound.png' alt="Page Not Found" className="img-fluid my-4" />
             <p>If you are not redirected, click <a href="/home" className="btn btn-primary btn-sm">here</a>.</p>
         </div>
